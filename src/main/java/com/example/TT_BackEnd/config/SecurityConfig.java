634911c20c623fa7f06a486ebc12f0df7b64a776
AzizChahlaoui <ingenieursirif@gmail.com>
2026-04-21 23:08:10 +0100

    fix CORS config for Vercel

--- src/main/java/com/example/TT_BackEnd/config/SecurityConfig.java
+++ src/main/java/com/example/TT_BackEnd/config/SecurityConfig.java
@@ -94,7 +94,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://tt-front-end-amber.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
